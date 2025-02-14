4b0ab0408c20e4700b5c5dbbf90b1fdfe361630c
Rakhman Imanov <raxman.i@mail.ru>
2025-02-14 20:17:50 +0300

    Выполнен доп 2 лабы

--- lab2/script.js
+++ lab2/script.js
@@ -9,23 +9,45 @@ xhr.onload = function () {
 
         let output = document.querySelector('.reminders');
         output.innerHTML = "";
-        
-        for (let reminder of reminders) {
-            let title = reminder.getElementsByTagName("title")[0].textContent;
-            let date = reminder.getElementsByTagName("date")[0].textContent;
 
-            const reminderItem = document.createElement('div');
-            reminderItem.className = "reminder-item";
-            reminderItem.innerHTML = `
-            <div class="reminder-body">
-                <input type="checkbox" name="" id="">
-                <div class="reminder-title">${title}</div>
-            </div>
-            <div class="reminder-footer">${date}</div>`
-
+        for (let i = 0; i < reminders.length; i++) {
+            console.log(i)
+            let reminderItem = getReminder(i, reminders);
             output.appendChild(reminderItem);
         }
+
+        let input = document.querySelector(".num");
+        if (input) {
+            input.onchange = function () {
+                let index = Number(input.value)
+                if (index || index === 0) {
+                    let numOutput = document.querySelector('.num-output');
+                    numOutput.innerHTML = '';
+                    let reminderItem = getReminder(index, reminders);
+                    numOutput.appendChild(reminderItem);
+                }
+            }
+        }
+
     } else {
-        console.log("Ошибка при открытии XML");
+        console.log("unable to open xml");
     }
+
+}
+
+function getReminder(index, reminders) {
+    let reminder = reminders[index]
+    let title = reminder.getElementsByTagName("title")[0].textContent;
+    let date = reminder.getElementsByTagName("date")[0].textContent;
+
+    const reminderItem = document.createElement('div');
+    reminderItem.className = "reminder-item";
+    reminderItem.innerHTML = `
+    <div class="reminder-body">
+        <input type="checkbox" name="" id="">
+        <a class="reminder-title" href="#">${title}</div>
+    </div>
+    <div class="reminder-footer">${date}</div>`
+
+    return reminderItem;
 }
